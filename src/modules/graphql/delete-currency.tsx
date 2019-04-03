bc8f6ebc215d52220faf272778445294f83ed05d
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-04-03 20:08:25 +0700

    Fix unit tests cause by useGraphql component

--- src/modules/graphql/delete-currency.tsx
+++ src/modules/graphql/delete-currency.tsx
@@ -14,29 +14,34 @@ export const DELETE_CURRENCY_MUTATION = gql`
 // I'm not going to actually render this component, because this
 // isn't a valid mutation on our sample server, and I don't want
 // to handle all the errors :)
-export default () => (
-  <Mutation mutation={DELETE_CURRENCY_MUTATION}>
-    {(mutate, { loading, error, data }) => {
-      if (loading) {
-        return <p>Loading...</p>;
-      }
-      if (error) {
-        return <p>Error :(</p>;
-      }
-      if (data) {
-        return <p>Deleted!</p>;
-      }
+export default () => {
+  // tslint:disable-next-line: no-empty
+  const onErrorHandler = useCallback(() => {}, []);
+  const onClickHandler = useCallback(
+    mutate => () => {
+      mutate({ variables: { name: 'USD' } });
+    },
+    []
+  );
+  return (
+    <Mutation mutation={DELETE_CURRENCY_MUTATION} onError={onErrorHandler}>
+      {(mutate, { loading, error, data }) => {
+        if (loading) {
+          return <p>Loading...</p>;
+        }
+        if (error) {
+          return <p>Error :(</p>;
+        }
+        if (data) {
+          return <p>Deleted!</p>;
+        }
 
-      return (
-        <button
-          onClick={useCallback(
-            () => mutate({ variables: { name: 'USD' } }),
-            []
-          )}
-        >
-          Click me to Delete the USD!
-        </button>
-      );
-    }}
-  </Mutation>
-);
+        return (
+          <button onClick={onClickHandler(mutate)}>
+            Click me to Delete the USD!
+          </button>
+        );
+      }}
+    </Mutation>
+  );
+};
