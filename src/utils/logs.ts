9aa261827b6e77209ab4d6a8d111b20ca87600ff
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-04-04 10:24:00 +0700

    Remove tslint stuff

--- src/utils/logs.ts
+++ src/utils/logs.ts
@@ -1,9 +1,7 @@
 export const logError = error => {
-  // tslint:disable-next-line:no-console
   console.error(error);
 };
 
 export const logInfo = error => {
-  // tslint:disable-next-line:no-console
   console.info(error);
 };
